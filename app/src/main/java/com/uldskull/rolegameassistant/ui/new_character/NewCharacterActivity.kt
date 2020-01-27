b21ff72faac2235b02296e9b00bcdec92bcf2909
pierre.antoine <pierre.antoine@CFD-L009>
2020-01-27 13:02:52 +0100

    Created newCharacterActivity layout file

---
+++ app/src/main/java/com/uldskull/rolegameassistant/ui/new_character/NewCharacterActivity.kt
@@ -0,0 +1,14 @@
+// File NewCharacterActivity.kt
+// @Author pierre.antoine - 27/01/2020 - No copyright.
+
+package com.uldskull.rolegameassistant.ui.new_character
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *   Class "NewCharacterActivity" :
+ *   TODO: Fill class use.
+ **/
+class NewCharacterActivity : AppCompatActivity() {
+// TODO : Fill class.
+}
