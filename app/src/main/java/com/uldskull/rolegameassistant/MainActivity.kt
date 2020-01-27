be4584e8d05048de6548901336012b16ac427cad
pierre.antoine <pierre.antoine@CFD-L009>
2020-01-27 15:59:08 +0100

    updated basic info.

--- app/src/main/java/com/uldskull/rolegameassistant/MainActivity.kt
+++ app/src/main/java/com/uldskull/rolegameassistant/MainActivity.kt
@@ -7,13 +7,13 @@ import com.uldskull.rolegameassistant.ui.new_character.NewCharacterActivity
 
 class MainActivity : AppCompatActivity() {
 
-
+    /** Activity Lifecycle  **/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //  Start new character activity.
         var intent = Intent(this, com.uldskull.rolegameassistant.ui.new_character.NewCharacterActivity::class.java)
-
         startActivity(intent)
     }
 }
