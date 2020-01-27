5a24e8a90a6b7f4f6ad825620342d93e9b734335
pierre.antoine <pierre.antoine@CFD-L009>
2020-01-27 13:34:09 +0100

    Setted koin

---
+++ app/src/main/java/com/uldskull/rolegameassistant/cross_cutting/AppModules.kt
@@ -0,0 +1,15 @@
+/** File AppModules.kt
+ *   @Author pierre.antoine - 27/01/2020 - No copyright.
+ **/
+
+package com.uldskull.rolegameassistant.cross_cutting
+
+import com.uldskull.rolegameassistant.ui.new_character.NewCharacterViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel { NewCharacterViewModel(
+        application = get()
+    )  }
+}
--- app/src/main/java/com/uldskull/rolegameassistant/ui/new_character/NewCharacterActivity.kt
+++ app/src/main/java/com/uldskull/rolegameassistant/ui/new_character/NewCharacterActivity.kt
@@ -3,7 +3,10 @@
 
 package com.uldskull.rolegameassistant.ui.new_character
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 /**
  *   Class "NewCharacterActivity" :
@@ -11,5 +14,15 @@ import androidx.appcompat.app.AppCompatActivity
  **/
 class NewCharacterActivity : AppCompatActivity() {
 
+    /** ViewModel for new character activity    **/
+    private lateinit var newCharacterViewModel:NewCharacterViewModel
+
+    /** Activity life cycle **/
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        //  Get the ViewModel by DI
+        newCharacterViewModel = getViewModel()
+    }
 
 }
