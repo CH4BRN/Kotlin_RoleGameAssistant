8f24a35254a2e0aaf8bc99d00fc4185fbba0bfda
CH4BRN <erreipantoine@gmail.com>
2020-02-11 17:30:03 +0100

    Skill repository

---
+++ domain_skill/src/main/java/com/uldskull/rolegameassistant/repository/SkillRepository.kt
@@ -0,0 +1,11 @@
+// SkillRepository.kt created by UldSkull - 11/02/2020
+
+package com.uldskull.rolegameassistant.repository
+
+/**
+Interface "SkillRepository"
+
+TODO : Describe interface utility.
+ **/
+interface SkillRepository {
+}
---
+++ infrastructure_data/src/main/java/com/uldskull/rolegameassistant/infrastructure/repositories/SkillRepositoryImpl.kt
@@ -0,0 +1,12 @@
+// SkillRepositoryImpl.kt created by UldSkull - 11/02/2020
+
+package com.uldskull.rolegameassistant.infrastructure.repositories
+
+import com.uldskull.rolegameassistant.repository.SkillRepository
+
+/**
+Class "SkillRepositoryImpl"
+
+TODO: Describe class utility.
+ */
+class SkillRepositoryImpl : SkillRepository
