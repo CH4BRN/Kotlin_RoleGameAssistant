1f2267d418f7e1b1cbd26e06097f21da9dbc5d29
CH4BRN <erreipantoine@gmail.com>
2020-02-11 18:00:12 +0100

    added Characteristics domain

---
+++ domain_characteristics/src/main/java/com/uldskull/rolegameassistant/model/DomainGender.kt
@@ -0,0 +1,13 @@
+// DomainGender.kt created by UldSkull - 11/02/2020
+
+package com.uldskull.rolegameassistant.model
+
+/**
+Class "DomainGender"
+
+Domain model for gender.
+ */
+data class DomainGender(
+    val pkGenderId: Long? = null,
+    val name: String? = "gender"
+)
