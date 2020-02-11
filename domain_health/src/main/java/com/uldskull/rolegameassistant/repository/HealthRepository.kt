8cf6deb8b3134c534d4b4be6af71c059abf1eaac
CH4BRN <erreipantoine@gmail.com>
2020-02-11 17:53:38 +0100

    added health domain

--- domain_health/src/main/java/com/uldskull/rolegameassistant/repository/HealthRepository.kt
+++ domain_health/src/main/java/com/uldskull/rolegameassistant/repository/HealthRepository.kt
@@ -1,13 +1,13 @@
-// SkillRepository.kt created by UldSkull - 11/02/2020
+// HealthRepository.kt created by UldSkull - 11/02/2020
 
 package com.uldskull.rolegameassistant.repository
 
 /**
-    Interface "SkillRepository"
+    Interface "HealthRepository"
 
     The repository class will be responsible for interacting with the Room database on behalf
     of the ViewModel and will need to provide methods that use the DAO to insert, delete and
     query basic info records.
  **/
-interface SkillRepository {
+interface HealthRepository {
 }
---
+++ infrastructure_data/src/main/java/com/uldskull/rolegameassistant/infrastructure/repositories/HealthRepositoryImpl.kt
@@ -0,0 +1,12 @@
+// HealthRepositoryImpl.kt created by UldSkull - 11/02/2020
+
+package com.uldskull.rolegameassistant.infrastructure.repositories
+
+import com.uldskull.rolegameassistant.repository.HealthRepository
+
+/**
+    Class "HealthRepositoryImpl"
+
+    Insert and get health from database.
+ */
+class HealthRepositoryImpl : HealthRepository
