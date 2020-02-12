1b51beb9325d0fec9fccaf026ef764e264205bdf
CH4BRN <erreipantoine@gmail.com>
2020-02-12 06:57:01 +0100

    added repository impl

--- infrastructure_data/src/main/java/com/uldskull/rolegameassistant/infrastructure/repositories/AbilityScoreRepositoryImpl.kt
+++ infrastructure_data/src/main/java/com/uldskull/rolegameassistant/infrastructure/repositories/AbilityScoreRepositoryImpl.kt
@@ -1,6 +1,6 @@
 // AbilityScoreRepositoryImpl.kt created by UldSkull - 12/02/2020
 
-package com.uldskull.rolegameassistant.infrastructure.database_model.abilityscore
+package com.uldskull.rolegameassistant.infrastructure.repositories
 
 import com.uldskull.rolegameassistant.repository.AbilityScoreRepository
 
