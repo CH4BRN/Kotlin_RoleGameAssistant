5ec1ce0d6fc19315dcdea67b469ca4370e70c11e
CH4BRN <erreipantoine@gmail.com>
2020-02-12 06:44:16 +0100

    added characteristics domain model

---
+++ domain_characteristics/src/main/java/com/uldskull/rolegameassistant/repository/CharacteristicsRepository.kt
@@ -0,0 +1,12 @@
+// CharacteristicsRepository.kt created by UldSkull - 12/02/2020
+
+package com.uldskull.rolegameassistant.repository
+
+/**
+    Interface "CharacteristicsRepository"
+
+    Contract to allow characteristics persistence.
+ **/
+interface CharacteristicsRepository {
+    // TODO : Fill interface.
+}
---
+++ infrastructure_data/src/main/java/com/uldskull/rolegameassistant/infrastructure/repositories/CharacteristicsRepositoryImpl.kt
@@ -0,0 +1,12 @@
+// CharacteristicsRepositoryImpl.kt created by UldSkull - 12/02/2020
+
+package com.uldskull.rolegameassistant.infrastructure.repositories
+
+import com.uldskull.rolegameassistant.repository.CharacteristicsRepository
+
+/**
+    Class "CharacteristicsRepositoryImpl"
+
+    Insert and get Characteristics from database.
+ */
+class CharacteristicsRepositoryImpl : CharacteristicsRepository
