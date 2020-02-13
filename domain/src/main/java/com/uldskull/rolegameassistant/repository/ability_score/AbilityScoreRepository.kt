9338c2c511b4db2d1291ab2b8fe64935f84d0c30
CH4BRN <erreipantoine@gmail.com>
2020-02-13 07:34:27 +0100

    Updated domain

--- domain/src/main/java/com/uldskull/rolegameassistant/repository/ability_score/AbilityScoreRepository.kt
+++ domain/src/main/java/com/uldskull/rolegameassistant/repository/ability_score/AbilityScoreRepository.kt
@@ -1,4 +1,4 @@
-// AbilityScoreRepositoryitory.kt created by UldSkull - 12/02/2020
+// AbilityScoreRepository.kt created by UldSkull - 12/02/2020
 
 package com.uldskull.rolegameassistant.repository.ability_score
 
@@ -8,7 +8,7 @@ import com.uldskull.rolegameassistant.repository.GenericRepository
 /**
     Interface "AbilityScoreRepository"
 
-    The com.uldskull.rolegameassistant.com.uldskull.rolegameassistant.repository class will be responsible for interacting with
+    The repository class will be responsible for interacting with
     the Room database on behalf of the ViewModel and will need to provide methods that use
     the DAO to insert, delete and query basic info records.
     With the exception of the getAllBasicInfo() DAO method
