feaf217a3afa7fa882ee02a39964330aa2107da4
CH4BRN <erreipantoine@gmail.com>
2020-02-15 11:34:11 +0100

    updated database model to add basic info

---
+++ infrastructure_data/src/main/java/com/uldskull/rolegameassistant/infrastructure/database_model/basic_info/DbRace.kt
@@ -0,0 +1,38 @@
+// DbRace.kt created by UldSkull - 15/02/2020
+
+package com.uldskull.rolegameassistant.infrastructure.database_model.basic_info
+
+import com.uldskull.rolegameassistant.infrastructure.database_model.DbEntity
+import com.uldskull.rolegameassistant.models.basic_info.DomainRace
+
+/**
+Class "DbRace"
+
+Database model for race.
+ */
+class DbRace(
+    val dbRaceId: Long? = null,
+    val dbRaceName: String? = "name"
+) : DbEntity<DomainRace> {
+    /**
+     * Converts into domain model.
+     */
+    override fun toDomain(): DomainRace {
+        return DomainRace(
+            raceId = dbRaceId,
+            raceName = dbRaceName
+        )
+    }
+
+    companion object {
+        /**
+         * Converts into database model
+         */
+        fun from(domainRace: DomainRace): DbRace {
+            return DbRace(
+                dbRaceId = domainRace.raceId,
+                dbRaceName = domainRace.raceName
+            )
+        }
+    }
+}
