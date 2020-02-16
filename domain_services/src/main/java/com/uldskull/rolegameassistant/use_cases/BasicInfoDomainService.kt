c25bb828e69d4616921003cad0b849fb65b726e2
CH4BRN <erreipantoine@gmail.com>
2020-02-16 17:57:19 +0100

    added domain services

---
+++ domain_services/src/main/java/com/uldskull/rolegameassistant/use_cases/BasicInfoDomainService.kt
@@ -0,0 +1,10 @@
+// BasicInfoDomainService.kt created by UldSkull - 16/02/2020
+
+package com.uldskull.rolegameassistant.use_cases
+
+/**
+Class "BasicInfoDomainService"
+
+TODO: Describe class utility.
+ */
+interface BasicInfoDomainService{}
---
+++ domain_services/src/main/java/com/uldskull/rolegameassistant/use_cases/BasicInfoDomainServiceImpl.kt
@@ -0,0 +1,12 @@
+// BasicInfoDomainService.kt created by UldSkull - 16/02/2020
+
+package com.uldskull.rolegameassistant.use_cases
+
+/**
+Class "BasicInfoDomainService"
+
+TODO: Describe class utility.
+ */
+class BasicInfoDomainServiceImpl(basicInfoApplicationService: BasicInfoDomainService) : BasicInfoDomainService{
+
+}
