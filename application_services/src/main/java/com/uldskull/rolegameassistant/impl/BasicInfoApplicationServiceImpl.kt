02c842d2fd8c01801e464102a89f80aa921d4370
pierre.antoine <pierre.antoine@CFD-L009>
2020-02-17 15:25:48 +0100

    Updated application services

--- application_services/src/main/java/com/uldskull/rolegameassistant/impl/BasicInfoApplicationServiceImpl.kt
+++ application_services/src/main/java/com/uldskull/rolegameassistant/impl/BasicInfoApplicationServiceImpl.kt
@@ -3,9 +3,13 @@
 package com.uldskull.rolegameassistant.impl
 
 import androidx.lifecycle.LiveData
+import com.uldskull.rolegameassistant.base.Response
 import com.uldskull.rolegameassistant.contracts.BasicInfoApplicationService
-import com.uldskull.rolegameassistant.contracts.repository.basic_info.BasicInfoRepository
 import com.uldskull.rolegameassistant.models.basic_info.DomainBasicInfo
+import com.uldskull.rolegameassistant.repository.basic_info.BasicInfoRepository
+import com.uldskull.rolegameassistant.useCases.UseCaseGetCounter
+import com.uldskull.rolegameassistant.usecases.counter.GetCounterRequest
+
 
 /**
 Class "BasicInfoApplicationServiceImpl"
@@ -13,8 +17,17 @@ Class "BasicInfoApplicationServiceImpl"
 TODO: Describe class utility.
  */
 class BasicInfoApplicationServiceImpl(
-    basicInforRepository: BasicInfoRepository<LiveData<DomainBasicInfo>>
-)
-    :  BasicInfoApplicationService{
+    basicInfoRepository: BasicInfoRepository<LiveData<DomainBasicInfo>>,
+    val useCaseGetCounter: UseCaseGetCounter
+) : BasicInfoApplicationService {
+    override suspend fun getCount(): Int? {
+        val request = GetCounterRequest()
+        val response = useCaseGetCounter.execute(request)
+        if (response is Response.Success) {
+            return response.data
+        }
+        return 0
+    }
+
 
 }
