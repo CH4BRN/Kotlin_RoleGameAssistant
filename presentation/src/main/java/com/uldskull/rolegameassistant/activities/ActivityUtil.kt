53d03da8e65bbe499dafdfacda60490117562b7b
CH4BRN <erreipantoine@gmail.com>
2020-02-25 08:11:35 +0100

    added character recycler view fragment

--- presentation/src/main/java/com/uldskull/rolegameassistant/activities/ActivityUtil.kt
+++ presentation/src/main/java/com/uldskull/rolegameassistant/activities/ActivityUtil.kt
@@ -7,13 +7,21 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
-    inline fun FragmentManager.doTransaction(func: FragmentTransaction.() -> Unit){
-        val fragmentTransaction = beginTransaction()
-        fragmentTransaction.func()
-        fragmentTransaction.commit()
-    }
+inline fun FragmentManager.doTransaction(func: FragmentTransaction.() -> Unit) {
+    val fragmentTransaction = beginTransaction()
+    fragmentTransaction.func()
+    fragmentTransaction.commit()
+}
 
 
-fun AppCompatActivity.replaceFragment(frameId:Int, fragment: Fragment){
-    supportFragmentManager.doTransaction{replace(frameId, fragment)}
+fun AppCompatActivity.replaceFragment(frameId: Int, fragment: Fragment) {
+    supportFragmentManager.doTransaction { replace(frameId, fragment) }
+}
+
+fun AppCompatActivity.addFragment(frameId: Int, fragment: Fragment) {
+    supportFragmentManager.doTransaction { add(frameId, fragment) }
+}
+
+fun AppCompatActivity.removeFragment(fragment: Fragment) {
+    supportFragmentManager.doTransaction { remove(fragment) }
 }
