da37e07b1d860cd8e34182a14ad9f466a421136c
pierre.antoine <pierre.antoine@CFD-L009>
2020-03-05 19:02:30 +0100

    Added derived values

---
+++ presentation/src/main/java/com/uldskull/rolegameassistant/fragments/fragment/skills/JobSkillsRecyclerViewFragment.kt
@@ -0,0 +1,30 @@
+// File JobSkillsRecyclerViewFragment.kt
+// @Author pierre.antoine - 05/03/2020 - No copyright.
+
+package com.uldskull.rolegameassistant.fragments.fragment.skills
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.uldskull.rolegameassistant.fragments.fragment.CustomFragment
+
+/**
+ *   Class "JobSkillsRecyclerViewFragment" :
+ *   TODO: Fill class use.
+ **/
+class JobSkillsRecyclerViewFragment(activity: Activity) : CustomFragment(activity) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return initializeView(inflater, container)
+    }
+
+    override fun initializeView(layoutInflater: LayoutInflater, container: ViewGroup?): View? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+// TODO : Fill class.
+}
